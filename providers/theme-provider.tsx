1aa832223c34ab3cbe199617fff7fbb64e3ef37b
Arshad <hussainarshad19059@gmail.com>
2025-08-25 20:16:22 +0530

    ecommerce admin

---
+++ providers/theme-provider.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { useEffect, useState } from "react";
+
+export function ThemeProvider({ children }: { children: React.ReactNode }) {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) {
+        return null;
+    }
+
+    return (
+        <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
+            {children}
+        </NextThemesProvider>
+    );
+}
---
+++ src/app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ModalProvider } from "../../providers/modal-provider";
+import { ToasterProvider } from "../../providers/toast-provider";
+import { ThemeProvider } from "../../providers/theme-provider";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Admin Dashboard",
+  description: "admin dashboard for ecommerce ",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider afterSignOutUrl="/">
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <ToasterProvider />
+          <ModalProvider />
+          <ThemeProvider>{children}</ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
